29c91af70b4a7cdc2ed6be25d7a40c1cab1c0bec
Vedang <vedangssjgod@gmail.com>
2023-02-26 20:30:28 +0530

    Basic GUI of Landing Page and Rules Made.Accepting the username.

---
+++ src/Login.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Login extends JFrame implements ActionListener{
+    JButton rules,exit;JTextField tfname;
+        Login(){
+            getContentPane().setBackground(Color.WHITE);
+            setSize(1200,600);
+            setLocation(200,50);
+            ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icons/login.jpeg"));
+            JLabel image=new JLabel(i1);
+            add(image);
+            setLayout(null);
+            image.setBounds(0,0,600,600);
+
+            JLabel heading=new JLabel("ACO");
+            heading.setBounds(780,60,100,50);
+            heading.setFont(new Font("Viner Hand ITC",Font.BOLD,40));
+            heading.setForeground(new Color(30,144,225));
+            add(heading);
+            JLabel name=new JLabel("Enter Your Name:");
+            name.setBounds(780,140,300,30);
+            name.setFont(new Font("Mongolian Baiti",Font.BOLD,18));
+            name.setForeground(new Color(30,144,225));
+            add(name);
+            tfname= new JTextField();
+            tfname.setBounds(720,220,300,30);
+            tfname.setFont(new Font("Times New Roman",Font.BOLD,18));
+            add(tfname);
+
+            rules=new JButton("Rules");
+            rules.setBounds(730,260,120,25);
+            rules.setBackground(new Color(30,144,254));
+            rules.setForeground(Color.white);
+            rules.addActionListener(this);
+            add(rules);
+
+            exit=new JButton("Exit");
+            exit.setBounds(895,260,120,25);
+            exit.setBackground(new Color(30,144,254));
+            exit.setForeground(Color.white);
+            exit.addActionListener(this);
+            add(exit);
+
+            setVisible(true);
+
+    }
+    public static void main(String[] args){
+        new Login();
+
+    }
+    public void actionPerformed(ActionEvent ae)
+    {
+        if(ae.getSource()==rules){
+            String name=tfname.getText();
+            setVisible(false);
+            new Rules(name);
+        }
+        else if(ae.getSource()==exit){
+            setVisible(false);
+        }
+    }
+
+}
---
+++ src/Rules.java
@@ -0,0 +1,58 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class Rules extends JFrame implements ActionListener{
+    String name;JButton exit,start;
+    Rules(String name) {
+        this.name=name;
+        getContentPane().setBackground(Color.white);
+        setSize(700,550);
+        setLocation(350,150);
+        setLayout(null);
+        JLabel heading=new JLabel("Welcome "+ name + " to ACO");
+        heading.setBounds(100,20,700,100);
+        heading.setFont(new Font("Viner Hand ITC",Font.BOLD,40));
+        heading.setForeground(new Color(30,144,225));
+        add(heading);
+        JLabel rules=new JLabel();
+        rules.setBounds(80,80,700,350);
+        rules.setFont(new Font("Tahoma",Font.PLAIN,18));
+        rules.setText(
+                "<html>"+
+                        "1. You are given only set amount of time" + "<br><br>" +
+                        "2. You get around 2 minute for each question" + "<br><br>" +
+                        "3. You May sit around with a rough page" + "<br><br>" +
+                        "4. Be Honest." + "<br><br>" +
+                        "<html>"
+        );
+        add(rules);
+
+        exit=new JButton("Exit");
+        exit.setBounds(320,360,120,25);
+        exit.setBackground(new Color(30,144,254));
+        exit.setForeground(Color.white);
+        exit.addActionListener(this);
+        add(exit);
+
+        start=new JButton("Start");
+        start.setBounds(170,360,120,25);
+        start.setBackground(new Color(30,144,254));
+        start.setForeground(Color.white);
+        start.addActionListener(this);
+        add(start);
+        setVisible(true);
+    }
+    public static void main(String[] args){
+        new Rules("User");
+    }
+    public void actionPerformed(ActionEvent ae){
+        if(ae.getSource()==start){
+
+        }
+        else if(ae.getSource()==exit){
+            setVisible(false);
+        }
+    }
+
+}
